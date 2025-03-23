e13ab797411db1efba99f4fbe647f6d7d1234de3
tortrakul-won <tortrakulwonglieng@gmail.com>
2025-03-23 13:38:42 +0700

    first draft of navigation DONE

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module kagoban.go
 
 go 1.24.1
 
-require github.com/charmbracelet/bubbletea v1.3.4
+require (
+	github.com/charmbracelet/bubbletea v1.3.4
+	github.com/charmbracelet/lipgloss v1.0.0
+	github.com/davecgh/go-spew v1.1.1
+)
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/lipgloss v1.0.0 // indirect
 	github.com/charmbracelet/x/ansi v0.8.0 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
--- main.go
+++ main.go
@@ -6,7 +6,12 @@ package main
 // dependencies.
 import (
 	"fmt"
+	"maps"
 	"os"
+	"slices"
+	"sort"
+	"strconv"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	lg "github.com/charmbracelet/lipgloss"
@@ -19,37 +24,77 @@ var systemStyle = lg.NewStyle().
 	Padding(2, 4)
 
 type models struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
-	termSize struct {
-		width  int
-		height int
-	}
+	UIControl   UIControl
+	Notes       []*Note
+	SectionData []Section
 }
 
 func initialModel() models {
-	return models{
-		// Our to-do list is a grocery list
-		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+	mockNotes := []*Note{} // Create a slice with capacity for 4 items
+	for i := range 4 {
+		mockNotes = append(mockNotes, NewMockNote("test"+strconv.Itoa(i), i))
+	}
 
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
+	return models{
+		Notes: mockNotes,
+		SectionData: []Section{
+			{ID: 0, Order: 0, Name: "Uncategorized"},
+		},
 	}
 }
 
 func (m models) Init() tea.Cmd {
 	// Just return `nil`, which means "no I/O right now, please."
+
+	/*
+		Maybe check if there is section that I otherwise create the uncategorized one.
+	*/
 	return nil
 }
 
 func (m models) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	/* TODO
+	Update display order
+	right now reorder is not supported. it is sort by time
+	*/
+
+	/*
+		Group Notes with the same ID to Display order
+	*/
+	m.UIControl.DisplayOrder = make(map[int][]*Note)
+	dp := m.UIControl.DisplayOrder
+	for _, notePtr := range m.Notes {
+		dp[notePtr.SectionID] = append(dp[notePtr.SectionID], notePtr)
+	}
+
+	/*
+		Lets think about the algo
+		concern:
+			0. There will be time where all order will be zero  <- this likely won't happen when you connect to database
+					so lets just skip this logic and just mock them
+					TODO: mock order data when init
+
+			1. element might get add later with zero value order
+					This also mean I should keep current Max value globally at least in that section
+
+	*/
+
+	// *** DO THIS when ADD and REORDER
+	// for sectionID, notes := range dp {
+	// 	currMax := slices.MaxFunc(notes, func(a, b *Note) int {
+	// 		return a.Order - b.Order
+	// 	}).Order
+
+	// 	sort.Slice(notes, func(i, j int) bool {
+	// 		return notes[i].DateCreated
+	// 	})
+	// }
+
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.termSize.height = msg.Height
-		m.termSize.width = msg.Width
+		m.UIControl.TermSize.Height = msg.Height
+		m.UIControl.TermSize.Width = msg.Width
 	// Is it a key press?
 	case tea.KeyMsg:
 
@@ -62,24 +107,38 @@ func (m models) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		// The "up" and "k" keys move the cursor up
 		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
+			if m.UIControl.RowCursor > 0 {
+				m.UIControl.RowCursor--
 			}
 
 		// The "down" and "j" keys move the cursor down
 		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
+			//TODO Scope len() to each section column << DONE?
+			if notes := dp[m.UIControl.SectionCursor]; m.UIControl.RowCursor < len(notes)-1 {
+				m.UIControl.RowCursor++
+			}
+
+		case "left", "h":
+			if m.UIControl.SectionCursor > 0 {
+				m.UIControl.SectionCursor--
+				m.UIControl.RowCursor = 0
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "right", "l":
+			//TODO Scope len() to each section column << DONE?
+			if m.UIControl.SectionCursor < len(dp)-1 {
+				m.UIControl.SectionCursor++
+				m.UIControl.RowCursor = 0
 			}
 
 		// The "enter" key and the spacebar (a literal space) toggle
 		// the selected state for the item that the cursor is pointing at.
 		case "enter", " ":
-			_, ok := m.selected[m.cursor]
+			sectionNotePtrs, ok := m.UIControl.DisplayOrder[m.UIControl.SectionCursor]
 			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
+				notePtr := sectionNotePtrs[m.UIControl.RowCursor]
+				notePtr.IsChecked = !notePtr.IsChecked
 			}
 		}
 	}
@@ -90,40 +149,79 @@ func (m models) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m models) View() string {
+
 	// The header
 	s := "What should we buy at the market?\n\n"
 
+	dpo := m.UIControl.DisplayOrder
+	sectionIDs := maps.Keys(dpo)
+
+	sectionList := []Section{}
+	for sid := range sectionIDs {
+
+		idx := slices.IndexFunc(m.SectionData, func(el Section) bool {
+			return el.ID == sid
+		})
+
+		if idx > -1 {
+			section := m.SectionData[idx]
+			sectionList = append(sectionList, section)
+		}
+	}
+	//now we have section data
+
+	// sort section id by SectionData.Order
+	sort.Slice(sectionList, func(i, j int) bool {
+		return sectionList[i].Order < sectionList[j].Order
+	})
+
+	slices.SortFunc(sectionList, func(a, b Section) int {
+		return a.Order - b.Order
+	})
+
 	// Iterate over our choices
-	for i, choice := range m.choices {
+	for i, section := range sectionList {
+		notesInSection := dpo[section.ID]
 
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
+		if len(notesInSection) == 0 {
+			continue
 		}
 
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
+		sortedNotes := slices.Clone(notesInSection)
+		slices.SortFunc(sortedNotes, func(a, b *Note) int {
+			return a.Order - b.Order
+		})
+
+		for _, note := range sortedNotes {
+			// Is the cursor pointing at this item?
+			cursor := " " // no cursor
+			if m.UIControl.RowCursor == note.Order && m.UIControl.SectionCursor == section.Order {
+				cursor = ">" // cursor!
+			}
+
+			// Is this item selected?
+			checked := " " // not selected
+			if note.IsChecked {
+				checked = "x"
+			}
+
+			tmpS := fmt.Sprintf("%s [%s] %s", cursor, checked, note.Content)
+
+			// Render the row
+			if m.UIControl.RowCursor == i {
+				tmpS = lg.NewStyle().Foreground(lg.Color("23")).Render(tmpS)
+			}
+
+			s += tmpS
+			s += "\n"
 		}
-
-		tmpS := fmt.Sprintf("%s [%s] %s", cursor, checked, choice)
-
-		// Render the row
-		if m.cursor == i {
-			tmpS = lg.NewStyle().Foreground(lg.Color("23")).Render(tmpS)
-		}
-
-		s += tmpS
-		s += "\n"
 	}
 
 	// The footer
 	s += "\nPress q to quit.\n"
 
 	// Send the UI for rendering
-	return systemStyle.Width(m.termSize.width - 3).Height(m.termSize.height - 5).Render(s)
+	return systemStyle.Width(m.UIControl.TermSize.Width - 3).Height(m.UIControl.TermSize.Height - 5).Render(s)
 
 }
 
@@ -132,5 +230,52 @@ func main() {
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
+	}
+}
+
+// Notes
+type Note struct {
+	ID          int       // Database ID, unique for each Note
+	Order       int       // Display order of the note
+	Content     string    // The content of the note
+	SectionID   int       // Pointer to the parent Section
+	DateUpdated time.Time // Timestamp when the note was last updated
+	DateCreated time.Time // Timestamp when the note was created
+	IsChecked   bool      // Is the note completed/checked?
+	IsDeleted   bool      // Flag for soft deletion
+}
+
+func NewNote(content string) *Note {
+	return &Note{
+		Content:     content,
+		DateUpdated: time.Now(),
+		DateCreated: time.Now(),
+	}
+}
+
+func NewMockNote(content string, order int) *Note {
+	return &Note{
+		Content:     content,
+		DateUpdated: time.Now(),
+		DateCreated: time.Now(),
+		Order:       order,
+	}
+}
+
+type Section struct {
+	ID    int    // Unique identifier for the Section
+	Order int    // Display order
+	Name  string // Section name
+}
+
+type UIControl struct {
+	IsDialogOpened bool            // Tracks if a dialog is open
+	LastUIBuffer   string          // Stores the last state or buffer for UI
+	DisplayOrder   map[int][]*Note // Map SectionIDs to corresponding Notes
+	RowCursor      int             // which to-do list item our cursor is pointing at in a section
+	SectionCursor  int             // which column(Section) our cursor is pointing at
+	TermSize       struct {        // terminal size. currently use for fullscreen
+		Width  int
+		Height int
 	}
 }
